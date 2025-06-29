643ff3ac815788c0b8f0bf2ffde7522a171c38cb
JModi16 <k5rac70j@students.codeinstitute.net>
2025-06-29 15:33:33 +0100

    added secret_key to settings and envy

--- codestar/env.py
+++ codestar/env.py
@@ -3,3 +3,4 @@ import os
 os.environ.setdefault(
 	"DATABASE_URL", "postgresql://neondb_owner:npg_uMcI0l7GokAL@ep-late-feather-a2avc9t0.eu-central-1.aws.neon.tech/rust_spoil_womb_6271"
 )
+os.environ.setdefault("SECRET_KEY", "Clement25!")
