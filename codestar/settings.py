f086c29d81105c2a7067b3d9df8acb5b59cff689
JModi16 <k5rac70j@students.codeinstitute.net>
2025-07-02 15:00:37 +0100

    edited models.py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2y502&r6=u9h=0^jb$(vc&0agc929-c*d=*m-8@m%01c*q#%lh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
 '.herokuapp.com'
